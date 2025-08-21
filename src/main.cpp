142aea18da512884fb527e2614a2671f4f88c2d1
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-08-21 15:04:04 -0300

    0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,105 @@
-#include <raylib-cpp.hpp>
+#include "raylib.h"
+#include "raymath.h"
 
-int main() {
-    
-    // Initialization
-    int screenWidth = 800;
-    int screenHeight = 450;
+int main()
+{
+    const int   windowDimensions[2]{ 512, 512 };
+    const char* windowTitle{ "Gabe's Top Down RPG Game" };
+    const Color mapTexture_color{ WHITE };
+    const int   target_fps{ 60 };
 
-    raylib::Color textColor(LIGHTGRAY);
-    raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-    
-    SetTargetFPS(60);
+    InitWindow(windowDimensions[0], windowDimensions[1], windowTitle);
+    SetTargetFPS(target_fps);
 
-    // Main game loop
-    while (!w.ShouldClose()) // Detect window close button or ESC key
+    Texture2D mapTexture = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2   mapPos{ 0.0, 0.0 };
+    float     speed{ 4.1 };
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2   knightPos{ windowDimensions[0] / 2.0f - (0.5f * knight.width / 6.0f),
+                       windowDimensions[1] / 2.0f - 4.0f * (0.5f * knight.height) };
+
+    Rectangle sourceRec{ 0.0f, 0.0f, knight.width / 6.0f, (float)knight.height };
+
+    Rectangle destRec{ knightPos.x,
+                       knightPos.y,
+                       (knight.width / 6.0f) * 4.0f,
+                       (float)knight.height * 4.0f };
+
+    Vector2 origin{ destRec.width / 2.0f, destRec.height / 2.0f };
+    int     frame       = 0;
+    float   runningTime = 0.0f;
+    float   updateTime  = 1.0f / 12.0f;
+
+    while (!WindowShouldClose())
     {
-        // Update
+        const float dt{ GetFrameTime() };
 
-        // TODO: Update your variables here
-
-        // Draw
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
+
+        ClearBackground(mapTexture_color);
+
+        Vector2 direction{};
+
+        if (IsKeyDown(KEY_A))
+        {
+            direction.x -= 1.0;
+        }
+
+        if (IsKeyDown(KEY_D))
+        {
+            direction.x += 1.0;
+        }
+
+        if (IsKeyDown(KEY_W))
+        {
+            direction.y -= 1.0;
+        }
+
+        if (IsKeyDown(KEY_S))
+        {
+            direction.y += 1.0;
+        }
+
+        if (Vector2Length(direction) != 0.0)
+        {
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+
+            runningTime += dt;
+            if (runningTime >= updateTime)
+            {
+                runningTime = 0.0f;
+
+                frame++;
+                if (frame > 5)
+                {
+                    frame = 0;
+                }
+
+                sourceRec.x = frame * (knight.width / 6.0f);
+            }
+
+            // Flip horizontally depending on x direction
+            if (direction.x < 0)
+            {
+                sourceRec.width = -(knight.width / 6.0f);
+            }
+            else if (direction.x > 0)
+            {
+                sourceRec.width = (knight.width / 6.0f);
+            }
+        }
+        else
+        {
+            frame       = 0;
+            sourceRec.x = 0.0f;
+        }
+
+        DrawTextureEx(mapTexture, mapPos, 0.0, 4.0, WHITE);
+        DrawTexturePro(knight, sourceRec, destRec, origin, 0.0, WHITE);
         EndDrawing();
     }
-
-    return 0;
+    UnloadTexture(knight);
+    UnloadTexture(mapTexture);
+    CloseWindow();
 }
